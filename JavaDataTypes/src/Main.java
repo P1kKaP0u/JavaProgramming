9240aabf12ebdd5e371bfb1ad4f9eb5f1346e4f8
Mustafa Kemal Aktaş <mustafaktaskemal@gmail.com>
2023-10-04 18:17:30 +0300

    Worked on DataTypes

---
+++ JavaDataTypes/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //integer number data types
+        int number = 1012345311;
+        short number1 = 12312;
+        byte number2 = 123;
+        long number3 = -1037192121;
+
+        //decimal number data types
+        double number4 = 123451231.123;
+
+        //single character
+        char character = 'a';
+        char character1 = '1';
+
+        //boolean data type
+        boolean ısActive = true;
+        boolean ısDeactive = false;
+
+    }
+}
