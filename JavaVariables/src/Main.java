a15a3918f078c28f88b439ba351c86001ec04d1c
Mustafa Kemal Aktaş <mustafaktaskemal@gmail.com>
2023-10-04 17:45:33 +0300

    Worked on Variables

---
+++ JavaVariables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int studentPopulation = 10;
+        String title = "Student census ";
+        System.out.println(title + studentPopulation);
+        System.out.println("Student census 10");
+
+    }
+}
